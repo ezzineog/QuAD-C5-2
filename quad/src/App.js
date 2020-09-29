9959afcaec93192d7455f8c5dc45b11896122af1
Slim Kasraoui <slim.kasraoui2020@gmail.com>
2020-09-29 22:02:26 +0100

    commit for pull

--- quad/src/App.js
+++ quad/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import axios from 'axios'
 // import browser router.
 // import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // import other components.
 // eslint-disable-next-line
 import { Navbar } from "./components/Navbar.js";
-import { UserNavBar } from "./UserNavBar.js";
+// import { UserNavBar } from "./UserNavBar.js";
 // import { }
 // import bootstrap.
 import "bootstrap/dist/css/bootstrap.css";
