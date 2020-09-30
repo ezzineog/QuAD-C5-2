f25202be7bccfe4a376bfba1773f5841cc27d866
Slim Kasraoui <slim.kasraoui2020@gmail.com>
2020-09-30 13:52:14 +0100

    Filter initiated and feed pop up done

--- quad/src/components/freelancer/HomePage/Feed.js
+++ quad/src/components/freelancer/HomePage/Feed.js
@@ -27,7 +27,7 @@ export default class Feed extends Component {
                         <CardText>job description</CardText>
                         <Button id='detailsBtn' onClick={this.toggle}>More details</Button>
                         <Modal isOpen={this.state.modal}>
-                            <ModalHeader >Modal title</ModalHeader>
+                            <ModalHeader >Job description</ModalHeader>
                             <ModalBody>
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             </ModalBody>
