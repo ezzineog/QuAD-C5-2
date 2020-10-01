16bf6efb53a47713b43a46e413bf91b9aa5a7ec7
GhassenEljday <ghassenjday2001@gmail.com>
2020-10-01 12:08:23 +0100

    company structure ready

---
+++ quad/src/components/company/Home/AddPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddPost extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hey i have to be add post for the company</h1>
+            </div>
+        );
+    }
+}
+
+export default AddPost;
---
+++ quad/src/components/company/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hey im profile !</h1>
+            </div>
+        );
+    }
+}
+
+export default Profile;
