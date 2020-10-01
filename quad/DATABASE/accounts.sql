46af49cd280d1909ae8b178742cc26fcaf793b75
ezzineog <ezzineahmedtaha@gmail.com>
2020-10-01 15:37:13 +0200

    onGoing...

--- quad/DATABASE/accounts.sql
+++ quad/DATABASE/accounts.sql
@@ -27,20 +27,34 @@ CREATE TABLE jobOffers (
     PRIMARY KEY (ID)
 );
 
-INSERT INTO users (id, FirstName ,LastName, Email, Password) VALUES (1,"Ahmed","Ezzine","QuiteB@Deadly.com","test");
-INSERT INTO users (id, FirstName ,LastName, Email, Password) VALUES (2,"Ahmed","Ezzine","QuiteB@Deadly.com","test");
--- to create the this DATABASE IN YOUR MYSQL DATABASE GUYSINSERT INTO signup (id, FirstName ,LastName, Email, Password) VALUES (1,"Ahmed","Ezzine","QuiteB@Deadly.com","test");
--- to create the this DATABASE IN YOUR MYSQL DATABASE GUYSINSERT INTO signup (id, FirstName ,LastName, Email, Password) VALUES (1,"Ahmed","Ezzine","QuiteB@Deadly.com","test");
+CREATE TABLE company (
+    id int NOT NULL AUTO_INCREMENT,
+    Name varchar(25),
+    Email varchar(25),
+    Password varchar(25),
+    JobOffers varchar(25),
+    Location varchar(25),
+    PhoneNumber varchar(25),
+    primary key (ID)
+);
 
 -- to create the this DATABASE IN YOUR MYSQL DATABASE GUYS
 -- copy this in the terminal mysql
 -- mysql -u root -p < accounts.sql
 
-
-
--- INSERT INTO describe (id, Age ,Avatar, Description,PhoneNumber, Skills) VALUES (2,17,"srcImageLink","QuiteBDeadly",2525753,"Skills");
--- INSERT INTO describe (id, Age ,Avatar, Description,PhoneNumber, Skills) VALUES (3,19,"srcImageLink","QuiteBDeadly",000666,"Skills");
+INSERT INTO users (id, FirstName ,LastName, Email, Password) VALUES (1,"Ahmed","Ezzine","QuiteB@Deadly.com","test");
+INSERT INTO users (id, FirstName ,LastName, Email, Password) VALUES (2,"Ahmed","Ezzine","QuiteB@Deadly.com","test");
 
 INSERT INTO jobOffers (id ,companyId,JobTitle,Description) VALUES (1,"companyName","CompanyTitle","789");
 INSERT INTO jobOffers (id ,companyId,JobTitle,Description) VALUES (2,"Ezzines company","test","freeelance");
 INSERT INTO jobOffers (id ,companyId,JobTitle,Description) VALUES (3,"7851","CompanyTitle","123");
+-- 
+INSERT INTO company (id, Name, Email, Password, JobOffers, Location,PhoneNumber) VALUES (1, 'TEST', 'TEST@GMAIL.COM', 'PWDTEST123', 'NOTHING', 'TUNISIA',785452);
+INSERT INTO company (id, Name, Email, Password, JobOffers, Location,PhoneNumber) VALUES (2, '2TEST', '2TEST@GMAIL.COM', '2PWDTEST123', '2NOTHING', '2TUNISIA',412287451);
+INSERT INTO company (id, Name, Email, Password, JobOffers, Location,PhoneNumber) VALUES (3, '3TT', '3-TEST@GMAIL.COM', '3-PWDTEST123', '3-NOTHING', '3-TUNISIA',552858);
+
+
+-- to create the this DATABASE IN YOUR MYSQL DATABASE GUYSINSERT INTO signup (id, FirstName ,LastName, Email, Password) VALUES (1,"Ahmed","Ezzine","QuiteB@Deadly.com","test");
+-- to create the this DATABASE IN YOUR MYSQL DATABASE GUYSINSERT INTO signup (id, FirstName ,LastName, Email, Password) VALUES (1,"Ahmed","Ezzine","QuiteB@Deadly.com","test");
+-- INSERT INTO describe (id, Age ,Avatar, Description,PhoneNumber, Skills) VALUES (2,17,"srcImageLink","QuiteBDeadly",2525753,"Skills");
+-- INSERT INTO describe (id, Age ,Avatar, Description,PhoneNumber, Skills) VALUES (3,19,"srcImageLink","QuiteBDeadly",000666,"Skills");
