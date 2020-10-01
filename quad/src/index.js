9ae0f11f6b6775b413cb87d6a0d9e2066d4647d5
ezzineog <ezzineahmedtaha@gmail.com>
2020-10-01 14:49:34 +0200

    the posts view and the Addpost for the company sec are ready jusgt missing some style and backEnd im working on them now

--- quad/src/index.js
+++ quad/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import AddPost from './components/company/Home/AddPost.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AddPost />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
